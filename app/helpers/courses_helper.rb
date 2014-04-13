87b248b83b36bc5b0d1e458f1ea0a8b38e24ce6a
Marius Melzer <marius@rasumi.net>
2014-04-13 16:20:15 +0200

    restructured add course page

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,10 @@
 module CoursesHelper
+  def link_to_add_course_date(name, f)
+    new_object = CourseDate.new
+    id = new_object.object_id
+    fields = f.fields_for(:course_dates, new_object, child_index: id) do |builder|
+      render("courses/course_date_fields", f: builder)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end

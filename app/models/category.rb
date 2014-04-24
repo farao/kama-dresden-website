534f5fe80236d8f779ecc9ca60a507cf781a7f37
Marius Melzer <marius@rasumi.net>
2014-04-25 00:12:45 +0200

    add uniqueness check to names of category and place

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :courses
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   #validates :color, presence: true
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :course_date
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :lat, presence: true
   validates :lon, presence: true
 
